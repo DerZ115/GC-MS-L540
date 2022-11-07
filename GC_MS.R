6fda5c449e72edd92d818f19d8d6c160190ce92e
Daniel Zimmermann <daniel.zimmermann.dz@gmail.com>
2022-11-07 15:02:33 +0100

    Refactoring

--- GC_MS.R
+++ GC_MS.R
@@ -5,53 +5,7 @@ library(rstatix)
 library(ggplot2)
 library(ggpubr)
 
-
-split.vec <- function(vec, sep = "") {
-  is.sep <- vec == sep
-  split(vec[!is.sep], cumsum(is.sep)[!is.sep])
-}
-
-
-
-parse_libsearch <- function(filename) {
-  rts <- c()
-  qualities <- c()
-  cas_n <- c()
-  compounds <- c()
-  
-  search_data <- readLines(filename)
-  search_data <- head(tail(search_data, -18), -3)
-  search_data <- trimws(search_data, whitespace="[ \t\r\n.]")
-  search_data <- split.vec(search_data, sep = "")
-  
-  for (entry in search_data) {
-    line1 <- entry[1]
-    rt <- as.double(unlist(strsplit(line1, "\\s+"))[2])
-    
-    cmp_starts <- grep(" \\d{1,6} \\d{1,7}-\\d{2}-\\d ", entry)
-    cmp_starts <- c(cmp_starts, length(entry)+1)
-    
-    rest <- entry[cmp_starts[1]:(cmp_starts[2]-1)]
-    tmp <- str_match(rest[1], "\\s+\\d{1,6} (\\d{1,7}-\\d{2}-\\d)\\s+(\\d+)")
-    
-    cas <- tmp[1,2]
-    quality <- tmp[1,3]
-    cmpd_name <- str_remove(rest, "\\s+\\d{1,6} (\\d{1,7}-\\d{2}-\\d)\\s+(\\d+)")
-
-    rts <- c(rts, rt)
-    qualities <- c(qualities, quality)
-    cas_n <- c(cas_n, cas)
-    compounds <- c(compounds, paste0(cmpd_name, collapse = ""))
-    
-    
-  }
-  
-  result <- data.frame(rts, cas_n, compounds, qualities)
-  colnames(result) <- c("Ret.Time", "CAS", "Compound", "Quality")
-  return(result)
-}
-
-
+source("lib_parse.R") # Load functions
 
 integ_file <- "data/Integration_report_L_540.xlsx"
 
@@ -90,6 +44,7 @@ for (i in 1:length(unique_compounds$cas)) {
 
 names(results) <- xlsx_sheets
 
+### Uncomment if running for the first time, to generate a new compound file
 # write.csv(unique_compounds, "compounds.csv", row.names=F)
 
 unique_compounds <- read_csv("compounds.csv", col_types = "ciccdd")
